57402a57f8259e04dbb5e4622051dd3a47f31624
arturvaleev <ValeevAE@loodsen.ru>
2025-02-11 21:04:39 +0300

    Added working files for lesson 5

---
+++ src/lesson_5/work/Car.java
@@ -0,0 +1,15 @@
+package lesson5.work;
+
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 11.02.2025
+ */
+
+public class Car {
+
+    String color;
+    int age;
+    float power;
+    int countWheels;
+    boolean isMirrorsExist;
+}
